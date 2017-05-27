3e1368686629a99dc17f9eb8d00d9b42b647500e
SJ <penguin418@naver.com>
2017-05-28 00:15:55 +0900

    기능 추가: 노트 태그/노트 타이트 수정 기능

--- app/src/main/java/com/example/penguin/timetagger/Fragments/NoteFragment.java
+++ app/src/main/java/com/example/penguin/timetagger/Fragments/NoteFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.*;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -36,11 +37,33 @@ public class NoteFragment extends Fragment {
         super.onCreate(savedInstanceState);
         View view = inflater.inflate(R.layout.content_note, container, false);
         Bundle bundle = this.getArguments();
+        String noteTitle;
+        String noteBody;
         if(bundle != null) {
             note = bundle.getParcelable("NOTE");
+            noteTitle = note.getTitle();
+            noteBody = note.getBody();
+        }else{
+            noteTitle = null;
+            noteBody = null;
         }
-        EditText et = (EditText)view.findViewById(R.id.edit_text);
-        et.setText(note.getBody(), TextView.BufferType.EDITABLE);
+        EditText etNoteTitle = (EditText)(getActivity()).findViewById(R.id.toolbar_et);
+        etNoteTitle.setText(noteTitle);
+        etNoteTitle.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == 4 || actionId == 6){
+                    System.out.println("");
+                    /* TODO: Database함수로 tag 업데이트 */
+                    return true;
+                }
+                return false;
+            }
+        });
+        EditText etNoteBody = (EditText)view.findViewById(R.id.edit_text);
+        etNoteBody.setText(noteBody, TextView.BufferType.EDITABLE);
+
+        view.requestFocus();
         setHasOptionsMenu(true);
         return view;
     }
@@ -64,10 +87,12 @@ public class NoteFragment extends Fragment {
         int id = item.getItemId();
 
         if (id == R.id.action_save) {
+	        EditText etNoteTitle = (EditText)(getActivity()).findViewById(R.id.toolbar_et);
             EditText et = (EditText)getActivity().findViewById(R.id.edit_text);
-            String body = et.getText().toString();
-
-            note.setBody(body);
+	        String noteTitle = etNoteTitle.getText().toString();
+            String noteBody = et.getText().toString();
+	        note.setTitle(noteTitle);
+            note.setBody(noteBody);
 
             if(note.getNoteID() == -1)
                 DatabaseHelper.insertNote(note);
